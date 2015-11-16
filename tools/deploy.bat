2b55702d883e811c98935a7f5dc938315904ea18
Bill Zissimopoulos <billziss@navimatics.com>
2015-11-15 19:30:29 -0800

    tools/deploy.bat: simply driver deployment batch file

---
+++ tools/deploy.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set DRIVER=winfsp-x64.sys
+set TARGET_MACHINE=WIN8DBG
+set TARGET_ACCOUNT=\Users\%USERNAME%\Downloads\winfsp\
+set TARGET=\\%TARGET_MACHINE%%TARGET_ACCOUNT%
+
+echo on
+cd %~dp0..
+mkdir %TARGET% 2>nul
+copy build\VStudio\build\Debug\%DRIVER% %TARGET%
+echo sc create winfsp type=filesys binPath=%%~dp0%DRIVER% >%TARGET%sc-create.bat
+echo sc delete winfsp >%TARGET%sc-delete.bat
