ca86b7d6c58fa0142f8f02ced78e944faa770539
Bill Zissimopoulos <billziss@navimatics.com>
2015-11-17 15:11:11 -0800

    tools/debug.bat: start windbg for winfsp debugging

---
+++ tools/debug.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set DebugWorkspace=winfsp
+set DebugPort=50000
+set DebugKey=win8.debug.net.key
+
+set RegKey="HKLM\SOFTWARE\Microsoft\Windows Kits\Installed Roots"
+set RegVal="KitsRoot10"
+reg query %RegKey% /v %RegVal% >nul 2>&1 || (echo Cannot find Windows Kit >&2 & exit /b 1)
+for /f "tokens=2,*" %%i in ('reg query %RegKey% /v %RegVal% ^| findstr %RegVal%') do (
+    set KitRoot="%%j"
+)
+start "winfsp" %KitRoot%\Debuggers\x64\windbg -W %DebugWorkspace% -k net:port=%DebugPort%,key=%DebugKey%
