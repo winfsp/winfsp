e60e63ada08446fae8dbc43a19c3f7dd27aeb496
Bill Zissimopoulos <billziss@navimatics.com>
2015-11-24 17:35:36 -0800

    sys: VS Editor sucks: strip CR's

--- inc/winfsp/fsctl.h
+++ inc/winfsp/fsctl.h
@@ -13,7 +13,7 @@
 extern const __declspec(selectany) GUID FspFsctlDeviceClassGuid =
     { 0x6f9d25fa, 0x6dee, 0x4a9d, { 0x80, 0xf5, 0xe9, 0x8e, 0x14, 0xf3, 0x5e, 0x54 } };
 // {B48171C3-DD50-4852-83A3-344C50D93B17}
-extern const __declspec(selectany) GUID FspFsvrtDeviceClassGuid = 
+extern const __declspec(selectany) GUID FspFsvrtDeviceClassGuid =
     { 0xb48171c3, 0xdd50, 0x4852, { 0x83, 0xa3, 0x34, 0x4c, 0x50, 0xd9, 0x3b, 0x17 } };
 
 #define FSP_FSCTL_DISK_DEVICE_NAME      "WinFsp.Disk"
