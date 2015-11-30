34974ac52d91136a4c2281644b89fc094475e1bc
Bill Zissimopoulos <billziss@navimatics.com>
2015-11-30 15:47:45 -0800

    tools: deploy.bat

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set CONFIG=Debug
-set DRIVER=winfsp-x64.sys
+set SUFFIX=x64
 set TARGET_MACHINE=WIN8DBG
 set TARGET_ACCOUNT=\Users\%USERNAME%\Downloads\winfsp\
 set TARGET=\\%TARGET_MACHINE%%TARGET_ACCOUNT%
@@ -9,6 +9,10 @@ set TARGET=\\%TARGET_MACHINE%%TARGET_ACCOUNT%
 echo on
 cd %~dp0..
 mkdir %TARGET% 2>nul
-copy build\VStudio\build\%CONFIG%\%DRIVER% %TARGET%
+for f in (winfsp-%SUFFIX%.sys winfsp-%SUFFIX%.dll winfsp-tests-%SUFFIX%.exe mirror-%SUFFIX%.exe) do (
+	copy build\VStudio\build\%CONFIG%\%%f %TARGET%
+)
 echo sc create WinFsp type=filesys binPath=%%~dp0%DRIVER% >%TARGET%sc-create.bat
+echo sc start WinFsp >%TARGET%sc-start.bat
+echo sc stop WinFsp >%TARGET%sc-stop.bat
 echo sc delete WinFsp >%TARGET%sc-delete.bat
