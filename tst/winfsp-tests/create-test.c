472759ae831c99702bef1aa1e50c36e78f598b14
Bill Zissimopoulos <billziss@navimatics.com>
2016-01-19 19:05:58 -0800

    winfsp-tests: create_test

--- tst/winfsp-tests/create-test.c
+++ tst/winfsp-tests/create-test.c
@@ -30,6 +30,11 @@ void create_dotest(ULONG Flags, PWSTR Prefix)
     ASSERT(INVALID_HANDLE_VALUE != Handle);
     CloseHandle(Handle);
 
+    Handle = CreateFileW(FilePath,
+        GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_HIDDEN, 0);
+    ASSERT(INVALID_HANDLE_VALUE != Handle);
+    CloseHandle(Handle);
+
     memfs_stop(memfs);
 }
 
