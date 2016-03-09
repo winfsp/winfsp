735f5d56be6c43b649e8f23f97f703df5d27ac52
Bill Zissimopoulos <billziss@navimatics.com>
2016-03-08 23:54:33 -0800

    winfsp-tests: rdwr-test: overlapped testing

--- tst/winfsp-tests/rdwr-test.c
+++ tst/winfsp-tests/rdwr-test.c
@@ -188,7 +188,195 @@ void rdwr_noncached_test(void)
     }
 }
 
+void rdwr_noncached_overlapped_dotest(ULONG Flags, PWSTR VolPrefix, PWSTR Prefix, ULONG FileInfoTimeout)
+{
+    void *memfs = memfs_start_ex(Flags, FileInfoTimeout);
+
+    HANDLE Handle;
+    BOOL Success;
+    WCHAR FilePath[MAX_PATH];
+    SYSTEM_INFO SystemInfo;
+    DWORD SectorsPerCluster;
+    DWORD BytesPerSector;
+    DWORD FreeClusters;
+    DWORD TotalClusters;
+    PVOID AllocBuffer[2], Buffer[2];
+    ULONG AllocBufferSize;
+    DWORD BytesTransferred;
+    OVERLAPPED Overlapped;
+
+    GetSystemInfo(&SystemInfo);
+
+    StringCbPrintfW(FilePath, sizeof FilePath, L"%s%s\\",
+        VolPrefix ? L"" : L"\\\\?\\GLOBALROOT", VolPrefix ? VolPrefix : memfs_volumename(memfs));
+
+    Success = GetDiskFreeSpaceW(FilePath, &SectorsPerCluster, &BytesPerSector, &FreeClusters, &TotalClusters);
+    ASSERT(Success);
+    AllocBufferSize = 16 * SystemInfo.dwPageSize;
+
+    AllocBuffer[0] = _aligned_malloc(AllocBufferSize, SystemInfo.dwPageSize);
+    AllocBuffer[1] = _aligned_malloc(AllocBufferSize, SystemInfo.dwPageSize);
+    ASSERT(0 != AllocBuffer[0] && 0 != AllocBuffer[1]);
+
+    srand((unsigned)time(0));
+    for (PUINT8 Bgn = AllocBuffer[0], End = Bgn + AllocBufferSize; End > Bgn; Bgn++)
+        *Bgn = rand();
+
+    Buffer[0] = (PVOID)((PUINT8)AllocBuffer[0] + BytesPerSector);
+    Buffer[1] = (PVOID)((PUINT8)AllocBuffer[1] + BytesPerSector);
+
+    StringCbPrintfW(FilePath, sizeof FilePath, L"%s%s\\file0",
+        Prefix ? L"" : L"\\\\?\\GLOBALROOT", Prefix ? Prefix : memfs_volumename(memfs));
+
+    memset(&Overlapped, 0, sizeof Overlapped);
+    Overlapped.hEvent = CreateEvent(0, TRUE, FALSE, 0);
+    ASSERT(0 != Overlapped.hEvent);
+
+    Handle = CreateFileW(FilePath,
+        GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0,
+        CREATE_NEW,
+        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED | FILE_FLAG_NO_BUFFERING | FILE_FLAG_DELETE_ON_CLOSE,
+        0);
+    ASSERT(INVALID_HANDLE_VALUE != Handle);
+
+    Overlapped.Offset = 0;
+    Success = WriteFile(Handle, Buffer[0], BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(BytesPerSector == BytesTransferred);
+
+    Overlapped.Offset = 2 * BytesPerSector;
+    Success = WriteFile(Handle, Buffer[0], BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(BytesPerSector == BytesTransferred);
+
+    Overlapped.Offset = 0;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(BytesPerSector == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Overlapped.Offset = 2 * BytesPerSector;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(BytesPerSector == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Overlapped.Offset = 2 * BytesPerSector;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], 2 * BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(BytesPerSector == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Overlapped.Offset = 3 * BytesPerSector;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError() || ERROR_HANDLE_EOF == GetLastError());
+    if (ERROR_HANDLE_EOF != GetLastError())
+    {
+        Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+        ASSERT(!Success && ERROR_HANDLE_EOF == GetLastError());
+    }
+    ASSERT(0 == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Overlapped.Offset = 0;
+    Success = WriteFile(Handle, Buffer[0], 2 * SystemInfo.dwPageSize, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize == BytesTransferred);
+
+    Overlapped.Offset = 0;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], 2 * SystemInfo.dwPageSize, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Buffer[0] = AllocBuffer[0];
+    Buffer[1] = AllocBuffer[0];
+
+    Overlapped.Offset = 0;
+    Success = WriteFile(Handle, Buffer[0], 2 * SystemInfo.dwPageSize, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize == BytesTransferred);
+
+    Overlapped.Offset = 0;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], 2 * SystemInfo.dwPageSize, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Overlapped.Offset = 0;
+    Success = WriteFile(Handle, Buffer[0], 2 * SystemInfo.dwPageSize + BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize + BytesPerSector == BytesTransferred);
+
+    Overlapped.Offset = 0;
+    memset(AllocBuffer[1], 0, AllocBufferSize);
+    Success = ReadFile(Handle, Buffer[1], 2 * SystemInfo.dwPageSize + BytesPerSector, &BytesTransferred, &Overlapped);
+    ASSERT(Success || ERROR_IO_PENDING == GetLastError());
+    Success = GetOverlappedResult(Handle, &Overlapped, &BytesTransferred, TRUE);
+    ASSERT(Success);
+    ASSERT(2 * SystemInfo.dwPageSize + BytesPerSector == BytesTransferred);
+    ASSERT(0 == memcmp(Buffer[0], Buffer[1], BytesTransferred));
+
+    Success = CloseHandle(Handle);
+    ASSERT(Success);
+
+    Success = CloseHandle(Overlapped.hEvent);
+    ASSERT(Success);
+
+    _aligned_free(AllocBuffer[0]);
+    _aligned_free(AllocBuffer[1]);
+
+    memfs_stop(memfs);
+}
+
+void rdwr_noncached_overlapped_test(void)
+{
+    if (NtfsTests)
+    {
+        WCHAR DirBuf[MAX_PATH] = L"\\\\?\\";
+        GetCurrentDirectoryW(MAX_PATH - 4, DirBuf + 4);
+        rdwr_noncached_overlapped_dotest(-1, L"C:", DirBuf, 0);
+    }
+    if (WinFspDiskTests)
+    {
+        rdwr_noncached_overlapped_dotest(MemfsDisk, 0, 0, 1000);
+        rdwr_noncached_overlapped_dotest(MemfsDisk, 0, 0, INFINITE);
+    }
+    if (WinFspNetTests)
+    {
+        rdwr_noncached_overlapped_dotest(MemfsNet, L"\\\\memfs\\share", L"\\\\memfs\\share", 1000);
+        rdwr_noncached_overlapped_dotest(MemfsNet, L"\\\\memfs\\share", L"\\\\memfs\\share", INFINITE);
+    }
+}
+
 void rdwr_tests(void)
 {
     TEST(rdwr_noncached_test);
+    TEST(rdwr_noncached_overlapped_test);
 }
