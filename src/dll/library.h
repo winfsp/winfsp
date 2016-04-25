968de4689c2c310d57c969a45d300bf4354515ca
Bill Zissimopoulos <billziss@navimatics.com>
2016-04-25 14:29:05 -0700

    inc,src,tst: add AGPLv3 notices

--- src/dll/library.h
+++ src/dll/library.h
@@ -3,6 +3,17 @@
  *
  * @copyright 2015-2016 Bill Zissimopoulos
  */
+/*
+ * This file is part of WinFsp.
+ *
+ * You can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * Licensees holding a valid commercial license may use this file in
+ * accordance with the commercial license agreement provided with the
+ * software.
+ */
 
 #ifndef WINFSP_DLL_LIBRARY_H_INCLUDED
 #define WINFSP_DLL_LIBRARY_H_INCLUDED
