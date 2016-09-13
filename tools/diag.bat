eca93bbdb314ebc0ba193065a575547dd76c7150
Bill Zissimopoulos <billziss@navimatics.com>
2016-09-13 10:24:14 -0700

    installer: add diag.bat file to installation

--- tools/diag.bat
+++ tools/diag.bat
@@ -2,16 +2,18 @@
 
 setlocal
 
-echo WINFSP AND SSHFS-WIN INSTALLATION DIRECTORIES
+echo WINFSP INSTALLATION DIRECTORY
 reg query HKLM\SOFTWARE\WinFsp /reg:32
-reg query HKLM\SOFTWARE\SSHFS-Win /reg:32
 echo.
 
-echo WINFSP NETWORK PROVIDER DLL AND SSHFS REGISTRATIONS
+echo WINFSP DLL REGISTRATIONS
 reg query HKLM\SYSTEM\CurrentControlSet\Control\NetworkProvider\Order
 reg query HKLM\SYSTEM\CurrentControlSet\Services\WinFsp.Np\NetworkProvider
 reg query HKLM\SYSTEM\CurrentControlSet\Services\EventLog\Application\WinFsp
-reg query HKLM\SYSTEM\CurrentControlSet\Services\WinFsp.Launcher\Services\sshfs
+echo.
+
+echo WINFSP LAUNCHER REGISTRATIONS
+reg query HKLM\SYSTEM\CurrentControlSet\Services\WinFsp.Launcher\Services /s
 echo.
 
 echo WINFSP FSD CONFIGURATION AND STATUS
@@ -20,7 +22,7 @@ sc qc WinFsp
 sc sdshow WinFsp
 echo.
 
-echo WINFSP.LAUNCHER CONFIGURATION AND STATUS
+echo WINFSP LAUNCHER SERVICE CONFIGURATION AND STATUS
 sc query WinFsp.Launcher
 sc qc WinFsp.Launcher
 sc sdshow WinFsp.Launcher
