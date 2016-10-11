bd952253fb87a514bfd5ad3a67710c16624072d0
Bill Zissimopoulos <billziss@navimatics.com>
2016-10-11 11:39:17 -0700

    cosmetic fix

--- tst/memfs/memfs-main.c
+++ tst/memfs/memfs-main.c
@@ -9,7 +9,6 @@
  * You can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 3 as published by the Free Software
  * Foundation.
-
  *
  * Licensees holding a valid commercial license may use this file in
  * accordance with the commercial license agreement provided with the
