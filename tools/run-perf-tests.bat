40b61f7667d22a626a6369cc224fce4fe6cf1d6d
Bill Zissimopoulos <billziss@navimatics.com>
2017-01-04 13:44:22 -0800

    tools: run-perf-tests: fix

--- tools/run-perf-tests.bat
+++ tools/run-perf-tests.bat
@@ -14,14 +14,16 @@ popd
 verifier /query | findstr winfsp >nul 2>nul
 if !ERRORLEVEL! equ 0 echo warning: verifier for winfsp is ON >&2
 
+set launchctl="%ProjRoot%\build\VStudio\build\%Configuration%\launchctl-x64.exe"
+set fsbench="%ProjRoot%\build\VStudio\build\%Configuration%\fsbench-x64.exe"
+
 if X%2==Xself (
-    launchctl-x64 start memfs64 testdsk "" M: >nul
+    "%launchctl%" start memfs64 testdsk "" M: >nul
     rem Cannot use timeout under cygwin/mintty: "Input redirection is not supported"
     waitfor 7BF47D72F6664550B03248ECFE77C7DD /t 3 2>nul
     cd M: >nul 2>nul || (echo === Unable to find drive M: >&2 & goto fail)
 )
 
-set fsbench="%ProjRoot%\build\VStudio\build\%Configuration%\fsbench-x64.exe"
 mkdir fsbench
 pushd fsbench
 
@@ -45,7 +47,7 @@ popd
 rmdir fsbench
 
 if X%2==Xself (
-    launchctl-x64 stop memfs64 testdsk >nul
+    "%launchctl%" stop memfs64 testdsk >nul
     rem Cannot use timeout under cygwin/mintty: "Input redirection is not supported"
     waitfor 7BF47D72F6664550B03248ECFE77C7DD /t 3 2>nul
 )
