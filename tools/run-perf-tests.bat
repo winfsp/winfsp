56873406e3a204965c9b18dc97a5daa3e53c9ff2
Bill Zissimopoulos <billziss@navimatics.com>
2017-01-04 16:04:43 -0800

    appveyor: run-perf-tests

--- tools/run-perf-tests.bat
+++ tools/run-perf-tests.bat
@@ -14,30 +14,56 @@ popd
 verifier /query | findstr winfsp >nul 2>nul
 if !ERRORLEVEL! equ 0 echo warning: verifier for winfsp is ON >&2
 
+set launchctl="%ProjRoot%\build\VStudio\build\%Configuration%\launchctl-x64.exe"
 set fsbench="%ProjRoot%\build\VStudio\build\%Configuration%\fsbench-x64.exe"
+
+if X%2==Xbaseline (
+    %launchctl% start memfs64 testdsk "" M: >nul
+    rem Cannot use timeout under cygwin/mintty: "Input redirection is not supported"
+    waitfor 7BF47D72F6664550B03248ECFE77C7DD /t 3 2>nul
+    cd M: >nul 2>nul || (echo === Unable to find drive M: >&2 & goto fail)
+)
+
 mkdir fsbench
 pushd fsbench
 
-for %%a in (1000 2000 3000 4000 5000) do (
+set OptFiles=1000 2000 3000 4000 5000
+if X%2==Xbaseline set OptFiles=10000
+for %%a in (%OptFiles%) do (
     call :csv %%a "%fsbench% --files=%%a file_*"
 )
 
-for %%a in (100 200 300 400 500) do (
+set OptRdwrCc=100 200 300 400 500
+if X%2==Xbaseline set OptRdwrCc=1000
+for %%a in (%OptRdwrCc%) do (
     call :csv %%a "%fsbench% --rdwr-cc=%%a rdwr_cc_*"
 )
 
-for %%a in (100 200 300 400 500) do (
+set OptRdwrNc=100 200 300 400 500
+if X%2==Xbaseline set OptRdwrNc=100
+for %%a in (%OptRdwrNc%) do (
     call :csv %%a "%fsbench% --rdwr-nc=%%a rdwr_nc_*"
 )
 
-for %%a in (100 200 300 400 500) do (
+set OptMmap=100 200 300 400 500
+if X%2==Xbaseline set OptMmap=1000
+for %%a in (%OptMmap%) do (
     call :csv %%a "%fsbench% --mmap=%%a mmap_*"
 )
 
 popd
 rmdir fsbench
 
+if X%2==Xbaseline (
+    %launchctl% stop memfs64 testdsk >nul
+    rem Cannot use timeout under cygwin/mintty: "Input redirection is not supported"
+    waitfor 7BF47D72F6664550B03248ECFE77C7DD /t 3 2>nul
+)
+
 exit /b 0
+
+:fail
+exit /b 1
 
 :csv
 set Iter=%1
