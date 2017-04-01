a5eadc50a766ad771f75226682dffef0a6ac73b1
Bill Zissimopoulos <billziss@navimatics.com>
2017-04-01 01:06:40 -0700

    add .net dll and project

---
+++ src/dotnet/FileSystem.cs
@@ -0,0 +1,24 @@
+/**
+ * @file dotnet/FileSystem.cs
+ *
+ * @copyright 2015-2017 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinFsp.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 3 as published by the Free Software
+ * Foundation.
+ *
+ * Licensees holding a valid commercial license may use this file in
+ * accordance with the commercial license agreement provided with the
+ * software.
+ */
+
+namespace Fsp {
+
+public class FileSystem
+{
+}
+
+}
