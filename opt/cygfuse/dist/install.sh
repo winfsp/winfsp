6d5401d9114de9af28899dd663b5f69b76f657da
Bill Zissimopoulos <billziss@navimatics.com>
2017-05-17 00:54:26 -0700

    cygfuse: add 32-bit package

--- opt/cygfuse/dist/install.sh
+++ opt/cygfuse/dist/install.sh
@@ -1,1 +1,7 @@
-tar -C/ -xaf fuse-2.8-*.tar.xz
+cd "$(dirname "$0")"
+case $(uname -m) in
+x86_64)
+    tar -C/ -xaf x64/fuse-2.8-*.tar.xz ;;
+*)
+    tar -C/ -xaf x86/fuse-2.8-*.tar.xz ;;
+esac
--- opt/cygfuse/dist/uninstall.sh
+++ opt/cygfuse/dist/uninstall.sh
@@ -1,1 +1,7 @@
-tar -taf fuse-2.8-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f
+cd "$(dirname "$0")"
+case $(uname -m) in
+x86_64)
+    tar -taf x64/fuse-2.8-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
+*)
+    tar -taf x86/fuse-2.8-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
+esac
