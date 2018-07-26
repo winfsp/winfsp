ec4197d8b7c8a4cf2010feed00a0e5a429f4df5b
Bill Zissimopoulos <billziss@navimatics.com>
2018-07-25 21:23:29 -0700

    opt: cygfuse: fix install scripts

--- opt/cygfuse/dist/install.sh
+++ opt/cygfuse/dist/install.sh
@@ -1,15 +1,16 @@
 cd "$(dirname "$0")"
 case $(uname -m) in
 x86_64)
-    tar -C/ -xaf x64/fuse-2.*.tar.xz
-    tar -C/ -xaf x64/fuse-3.*.tar.xz
+    tar -C/ -xaf x64/fuse-*.tar.xz
+    tar -C/ -xaf x64/fuse3-*.tar.xz
     ;;
 i686)
-    tar -C/ -xaf x86/fuse-2.*.tar.xz
-    tar -C/ -xaf x86/fuse-3.*.tar.xz
+    tar -C/ -xaf x86/fuse-*.tar.xz
+    tar -C/ -xaf x86/fuse3-*.tar.xz
     ;;
 *)
     echo unsupported architecture 1>&2
     exit 1
+    ;;
 esac
 echo FUSE for Cygwin installed.
--- opt/cygfuse/dist/uninstall.sh
+++ opt/cygfuse/dist/uninstall.sh
@@ -1,13 +1,16 @@
 cd "$(dirname "$0")"
 case $(uname -m) in
 x86_64)
-    tar -taf x64/fuse-2.*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
-    tar -taf x64/fuse-3.*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
+    tar -taf x64/fuse-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f
+    tar -taf x64/fuse3-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f
+    ;;
 i686)
-    tar -taf x86/fuse-2.*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
-    tar -taf x86/fuse-3.*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f ;;
+    tar -taf x86/fuse-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f
+    tar -taf x86/fuse3-*.tar.xz | sed -e '/\/$/d' -e 's/.*/\/&/' | xargs rm -f
+    ;;
 *)
     echo unsupported architecture 1>&2
     exit 1
+    ;;
 esac
 echo FUSE for Cygwin uninstalled.
