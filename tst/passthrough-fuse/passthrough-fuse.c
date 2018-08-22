75ae8daf8f06668a43ab1b0bc92189ea6052ca2c
Bill Zissimopoulos <billziss@navimatics.com>
2018-08-21 18:09:33 -0700

    update commercial disclaimer

--- tst/passthrough-fuse/passthrough-fuse.c
+++ tst/passthrough-fuse/passthrough-fuse.c
@@ -10,9 +10,13 @@
  * General Public License version 3 as published by the Free Software
  * Foundation.
  *
- * Licensees holding a valid commercial license may use this file in
- * accordance with the commercial license agreement provided with the
- * software.
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the GPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
  */
 
 #include <errno.h>
