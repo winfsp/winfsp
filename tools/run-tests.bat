17d687fe7e4d23f5c2be5e67069fd071c7faf40a
Bill Zissimopoulos <billziss@navimatics.com>
2018-11-05 14:14:21 -0800

    tools: run-tests: disable create_readonlydir_test on compat FUSE tests (no FileAttributes)

--- tools/run-tests.bat
+++ tools/run-tests.bat
@@ -923,7 +923,7 @@ cd L: >nul 2>nul || (echo Unable to find drive L: >&2 & goto fail)
 L:
 "%ProjRoot%\build\VStudio\build\%Configuration%\%3.exe" ^
     --external --resilient --case-insensitive-cmp --share-prefix="\%1\%TMP::=$%\%1\test" ^
-    -create_fileattr_test -create_allocation_test -create_notraverse_test -create_backup_test -create_restore_test -create_namelen_test ^
+    -create_fileattr_test -create_readonlydir_test -create_allocation_test -create_notraverse_test -create_backup_test -create_restore_test -create_namelen_test ^
     -getfileinfo_name_test -setfileinfo_test -delete_access_test -delete_mmap_test -rename_flipflop_test -rename_mmap_test -setsecurity_test -querydir_namelen_test -exec_rename_dir_test ^
     -reparse* -stream*
 if !ERRORLEVEL! neq 0 set RunSampleTestExit=1
