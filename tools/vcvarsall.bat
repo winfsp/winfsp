191c98bd4194dfab43d742c886a44a69ff25fc3e
Bill Zissimopoulos <billziss@navimatics.com>
2020-05-21 18:59:01 -0700

    tools: vcvarsall.bat: minor fix

--- tools/vcvarsall.bat
+++ tools/vcvarsall.bat
@@ -2,7 +2,7 @@
 
 set vcvarsall="%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-if exist "%vswhere%" (
+if exist %vswhere% (
     for /f "usebackq tokens=*" %%i in (`%vswhere% -find VC\**\vcvarsall.bat`) do (
         set vcvarsall="%%i"
     )
