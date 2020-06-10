dcaa24bc52c7b286ec4173b6f9412d39c381a73c
Bill Zissimopoulos <billziss@navimatics.com>
2020-06-09 18:09:56 -0700

    tools: deploy.bat, debug.bat

--- tools/debug.bat
+++ tools/debug.bat
@@ -12,4 +12,4 @@ reg query %RegKey% /v %RegVal% >nul 2>&1 || (echo Cannot find Windows Kit >&2 & 
 for /f "tokens=2,*" %%i in ('reg query %RegKey% /v %RegVal% ^| findstr %RegVal%') do (
     set KitRoot="%%j"
 )
-start "winfsp" %KitRoot%\Debuggers\x64\windbg -W %DebugWorkspace% -k net:port=%DebugPort%,key=%DebugKey%
+start "%DebugWorkspace%" %KitRoot%\Debuggers\x64\windbg -W %DebugWorkspace% -k net:port=%DebugPort%,key=%DebugKey%
--- tools/deploy.bat
+++ tools/deploy.bat
@@ -11,12 +11,33 @@ set Chkpnt=winfsp
 if not X%1==X set Target=%1
 if not X%2==X set Chkpnt=%2
 
+(
+    echo regsvr32 /s winfsp-x64.dll
+) > %~dp0..\build\VStudio\build\%Config%\deploy-setup.bat
+
 set Files=
 for %%f in (winfsp-%Suffix%.sys winfsp-%Suffix%.dll winfsp-tests-%Suffix%.exe memfs-%Suffix%.exe) do (
     if [!Files!] == [] (
         set Files='%~dp0..\build\VStudio\build\%Config%\%%f'
     ) else (
         set Files=!Files!,'%~dp0..\build\VStudio\build\%Config%\%%f'
+    )
+)
+set Files=
+for %%f in (
+    %~dp0..\build\VStudio\build\%Config%\
+        winfsp-%Suffix%.sys
+        winfsp-%Suffix%.dll
+        winfsp-tests-%Suffix%.exe
+        memfs-%Suffix%.exe
+        deploy-setup.bat
+    ) do (
+    set File=%%~f
+    if [!File:~-1!] == [\] (
+        set Dir=!File!
+    ) else (
+        if not [!Files!] == [] set Files=!Files!,
+        set Files=!Files!'!Dir!!File!'
     )
 )
 
