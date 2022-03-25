9ecb6541cfbfecd2f9f419e03df24cb01d610c63
Bill Zissimopoulos <billziss@navimatics.com>
2022-03-25 15:37:18 +0000

    tools: vsvarsall.bat: cleanup and restrict to VS2019

--- tools/vcvarsall.bat
+++ tools/vcvarsall.bat
@@ -1,16 +1,24 @@
 @echo off
 
-set vcvarsall="%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"
+set vcvarsall=""
+
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 if exist %vswhere% (
-    for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -find VC\**\vcvarsall.bat`) do (
-        if exist "%%i" (
-            set vcvarsall="%%i"
-        ) else (
-            for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -property installationPath`) do (
-                set vcvarsall="%%i\VC\Auxiliary\Build\vcvarsall.bat"
-            )
+    for /f "usebackq tokens=*" %%i in (`%vswhere% -version [15.0^,17.0^) -find VC\**\vcvarsall.bat`) do (
+        set vcvarsall="%%i"
+    )
+)
+
+if not exist %vcvarsall% (
+    if exist %vswhere% (
+        for /f "usebackq tokens=*" %%i in (`%vswhere% -version [15.0^,17.0^) -property installationPath`) do (
+            set vcvarsall="%%i\VC\Auxiliary\Build\vcvarsall.bat"
         )
     )
 )
+
+if not exist %vcvarsall% (
+    set vcvarsall="%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"
+)
+
 call %vcvarsall% %*
