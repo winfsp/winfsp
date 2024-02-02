f16e8be116d8f00f5e392ade916d86dc58a06385
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2024-02-02 01:35:16 -0800

    Update msvc-build.bat

--- msvc-build.bat
+++ msvc-build.bat
@@ -1,8 +1,14 @@
 setlocal
+
+pushd build\VStudio\dotnet
+dotnet add package WixToolset.WcaUtil --version 4.0.3
+popd
+
 pushd "%~dp0"
 set "Configuration=%1"
 if [%Configuration%]==[] set Configuration=Debug
 rmdir /s /q build\VStudio\build
+rmdir /s /q build\VStudio\installer\CustomActions\build
 del /f build\VStudio\UpgradeLog*.htm
 del /f build\VStudio\driver.ddf
 
